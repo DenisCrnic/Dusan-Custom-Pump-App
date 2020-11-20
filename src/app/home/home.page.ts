933193e66216aa21c3f456a86f7addd64b1d7187
User <crnicdenis@gmail.com>
2020-11-20 21:05:05 +0100

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
+})
+export class HomePage {
+  app_name: string = "Dušan Custom Pump App";
+  constructor() {}
+}
